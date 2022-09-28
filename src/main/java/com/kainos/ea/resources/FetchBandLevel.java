fd07dcb658ebee3dcb18dc1767cb49deab13e4c3
JennicaMistry <113026083+JennicaMistry@users.noreply.github.com>
2022-09-28 11:51:18 +0100

    Update FetchBandLevel.java

--- src/main/java/com/kainos/ea/resources/FetchBandLevel.java
+++ src/main/java/com/kainos/ea/resources/FetchBandLevel.java
@@ -1,15 +1,18 @@
 package com.kainos.ea.resources;
+
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+
 import static com.kainos.ea.database.DataBaseConnection.getConnection;
 
 public class FetchBandLevel {
 
     private static Connection myConnection;
+    
     public static List<Band> getBand(){
         List<Band> baseList = new ArrayList();
         ResultSet resultSet = null;
