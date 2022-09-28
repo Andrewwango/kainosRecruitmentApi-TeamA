49090c9d135d7d52fa1047d033e09422a8b5ef62
kewin.taoanh <kewin.taoanh@kainos.com>
2022-09-28 14:47:36 +0200

    add JobRole class, add job-specification endpoint

--- src/main/java/com/kainos/ea/WebService.java
+++ src/main/java/com/kainos/ea/WebService.java
@@ -2,7 +2,9 @@ package com.kainos.ea;
 
 import com.kainos.ea.resources.Band;
 import com.kainos.ea.resources.BandLevel;
+import dao.SpecificationLevel;
 import io.swagger.annotations.Api;
+import models.JobRole;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
@@ -11,6 +13,14 @@ import java.util.List;
 @Path("/api")
 @Api("jobRoles")
 public class WebService {
+
+    @GET
+    @Path("/job-specification/{jobRoleId}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public JobRole getJobRole(@PathParam("jobRoleId") int jobRoleId){
+        JobRole jobRole = SpecificationLevel.getJobRole(jobRoleId);
+        return jobRole;
+    }
 
     @GET
     @Path("/viewBandLevel")
---
+++ src/main/java/models/JobRole.java
@@ -0,0 +1,54 @@
+package models;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class JobRole {
+    private int jobRoleId;
+    private String roleName;
+    private String specification;
+    private String link;
+
+    public int getJobRoleId() {
+        return jobRoleId;
+    }
+
+    public void setJobRoleId(int jobRoleId) {
+        this.jobRoleId = jobRoleId;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public String getSpecification() {
+        return specification;
+    }
+
+    public void setSpecification(String specification) {
+        this.specification = specification;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    @JsonCreator
+    public JobRole(
+            @JsonProperty("roleName") String name,
+            @JsonProperty("specification") String specification,
+            @JsonProperty("link") String link)
+    {
+        this.setRoleName(name);
+        this.setSpecification(specification);
+        this.setLink(link);
+    }
+}
