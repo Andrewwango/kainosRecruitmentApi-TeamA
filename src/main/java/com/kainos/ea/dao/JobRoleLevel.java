63444b9ee0b5a49d2fa472cb478c2195d8fa7629
DawidG <dawid.grzeszczak@kainos.com>
2022-09-28 15:41:53 +0200

    Working api for user story 1 added

---
+++ src/main/java/com/kainos/ea/dao/JobRoleLevel.java
@@ -0,0 +1,35 @@
+package com.kainos.ea.dao;
+
+import com.kainos.ea.models.JobRole;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.kainos.ea.database.DataBaseConnection.getConnection;
+
+public class JobRoleLevel {
+    public static List<JobRole> getJobRoles() throws SQLException {
+        List<JobRole> jobs = new ArrayList<>();
+        try {
+            Connection myConnection = getConnection();
+            Statement st = myConnection.createStatement();
+            ResultSet rs = st.executeQuery(
+                    "SELECT * " + "FROM jobRoles;");
+            while (rs.next()) {
+                JobRole job = new JobRole(
+                        rs.getString("roleName"),
+                        rs.getString("specification"),
+                        rs.getString("link")
+                );
+                jobs.add(job);
+            }
+        } catch (SQLException e) {
+            throw e;
+        }
+        return jobs;
+    }
+}
