2ddb0c45c9c61164585009517abb17b6e54db658
DawidG <dawid.grzeszczak@kainos.com>
2022-09-28 15:03:23 +0200

    working api for user story 1 added

--- src/main/java/com/kainos/ea/WebService.java
+++ src/main/java/com/kainos/ea/WebService.java
@@ -1,16 +1,25 @@
 package com.kainos.ea;
 
+import com.kainos.ea.database.DataBaseConnection;
 import com.kainos.ea.resources.Band;
 import com.kainos.ea.resources.BandLevel;
+import com.kainos.ea.resources.JobRole;
+import com.kainos.ea.resources.JobRoleLevel;
 import io.swagger.annotations.Api;
+import org.eclipse.jetty.http.HttpStatus;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.sql.SQLException;
 import java.util.List;
+
 
 @Path("/api")
 @Api("jobRoles")
 public class WebService {
+
+
 
     @GET
     @Path("/viewBandLevel")
@@ -18,5 +27,12 @@ public class WebService {
     public List<Band> getBases(){
         List<Band> bases = BandLevel.getBand();
         return bases;
+    }
+    @GET
+    @Path("/job-roles")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<JobRole> getJobRoles() throws SQLException {
+        List<JobRole> job_roles = JobRoleLevel.getJobRoles();
+        return job_roles;
     }
 }
