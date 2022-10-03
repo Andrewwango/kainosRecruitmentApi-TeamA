a5d5a867b550068a7cfb437ec5b18173cc688257
kewin.taoanh <kewin.taoanh@kainos.com>
2022-10-03 15:05:27 +0200

    add assert

--- src/test/java/com/kainos/ea/dao/SpecificationLevelTest.java
+++ src/test/java/com/kainos/ea/dao/SpecificationLevelTest.java
@@ -1,12 +1,15 @@
 package com.kainos.ea.dao;
 
 import com.kainos.ea.models.JobRole;
+import org.hamcrest.CoreMatchers;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import java.sql.SQLException;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.hibernate.validator.internal.util.Contracts.assertTrue;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertSame;
 
 @ExtendWith(MockitoExtension.class)
 public class SpecificationLevelTest {
@@ -26,5 +29,6 @@ public class SpecificationLevelTest {
     //Then
     assertThat(result).isNotNull();
     assertFalse(result.getSpecification() == null);
+    assertThat(result.getRoleName()).contains("Data Analyst");
   }
 }
