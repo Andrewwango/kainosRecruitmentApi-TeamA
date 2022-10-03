595cebc067206f9a1194e54e779f8012b57c62f8
kewin.taoanh <kewin.taoanh@kainos.com>
2022-10-03 17:04:43 +0200

    remove imports

--- src/test/java/com/kainos/ea/dao/SpecificationLevelTest.java
+++ src/test/java/com/kainos/ea/dao/SpecificationLevelTest.java
@@ -1,21 +1,13 @@
 package com.kainos.ea.dao;
 
 import com.kainos.ea.models.JobRole;
-import org.hamcrest.CoreMatchers;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
 import java.sql.SQLException;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.hibernate.validator.internal.util.Contracts.assertTrue;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertSame;
 
-@ExtendWith(MockitoExtension.class)
 public class SpecificationLevelTest {
-
   private SpecificationLevel specificationLevel;
-
   @Test
   void jobRoleLevel_ReturnsJobRoleById_WhenCalledGetJobRole() throws SQLException {
     //Given
