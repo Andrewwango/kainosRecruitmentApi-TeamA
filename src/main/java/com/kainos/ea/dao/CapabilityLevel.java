f90b15f7f786826cc130a8f40a3404875f6c7520
kingsley.osime <kingsleyo@kainos.com>
2022-10-03 09:29:33 +0100

    refactored, tests added

--- src/main/java/com/kainos/ea/dao/CapabilityLevel.java
+++ src/main/java/com/kainos/ea/dao/CapabilityLevel.java
@@ -1,5 +1,6 @@
 package com.kainos.ea.dao;
 
+import com.kainos.ea.utils.DataBaseConnection;
 import com.kainos.ea.models.Capability;
 
 import java.sql.Connection;
@@ -8,25 +9,25 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
-import static com.kainos.ea.database.DataBaseConnection.getConnection;
 
 public class CapabilityLevel {
-
-    private static Connection myConnection;
 
     public static List<Capability> getCapabilities(){
         List<Capability> capList = new ArrayList();
         ResultSet resultSet = null;
 
         try{
-            Connection myConnection = getConnection();
+            DataBaseConnection dataBaseConnection = new DataBaseConnection();
+            Connection myConnection = dataBaseConnection.getConnection();
             Statement st = myConnection.createStatement();
             resultSet = st.executeQuery("SELECT roleName, capabilityName FROM jobRoles JOIN capabilities WHERE jobRoles.jobRoleID=capabilities.jobRoleID");
 
             while (resultSet.next()) {
-                Capability cap = new Capability(
-                        resultSet.getString("roleName"),
-                        resultSet.getString("capabilityName"));
+                Capability cap = Capability.builder()
+                    .capabilityName(resultSet.getString("capabilityName"))
+                    .roleName(resultSet.getString("roleName"))
+                    .build();
+
                 capList.add(cap);
             }
         } catch (SQLException e) {
--- src/main/java/com/kainos/ea/dao/SpecificationLevel.java
+++ src/main/java/com/kainos/ea/dao/SpecificationLevel.java
@@ -1,11 +1,11 @@
 package com.kainos.ea.dao;
 
+import com.kainos.ea.utils.DataBaseConnection;
 import com.kainos.ea.models.JobRole;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import static com.kainos.ea.database.DataBaseConnection.getConnection;
 
 public class SpecificationLevel {
 
@@ -14,20 +14,22 @@ public class SpecificationLevel {
         ResultSet resultSet;
 
         try{
-            Connection myConnection = getConnection();
+            DataBaseConnection dataBaseConnection = new DataBaseConnection();
+            Connection myConnection = dataBaseConnection.getConnection();
             Statement st = myConnection.createStatement();
 
             resultSet = st.executeQuery("SELECT * FROM jobRoles WHERE jobRoleID = " + jobRoleId + ";");
 
             while (resultSet.next()) {
-                JobRole jobRole = new JobRole(
-                        resultSet.getInt("jobRoleID"),
-                        resultSet.getString("roleName"),
-                        resultSet.getString("specification"),
-                        resultSet.getString("link"),
-                        resultSet.getInt("bandID"),
-                        resultSet.getInt("capabilityID")
-                );
+                JobRole jobRole = JobRole.builder()
+                    .jobRoleId(resultSet.getInt("jobRoleID"))
+                    .roleName(resultSet.getString("roleName"))
+                    .specification(resultSet.getString("specification"))
+                    .link(resultSet.getString("link"))
+                    .bandID(resultSet.getInt("bandID"))
+                    .capabilityID(resultSet.getInt("capabilityID"))
+                    .build();
+
                 jobRole.setJobRoleId(jobRoleId);
                 return jobRole;
             }
